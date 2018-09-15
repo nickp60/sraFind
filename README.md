651e611d16d6d2c330b6e8d733c26b81c1d52443
nickp60 <nickp60@gmail.com>
2018-09-15 15:25:31 +0100

    update fetcher scripts; remove python version

--- README.md
+++ README.md
@@ -1,7 +1,48 @@
-#
+# sraFind
+
+sraFind is a tool to gauge the completeness of assembly and availaility of raw data from prokaryotic genomes.
+
+In short it uses a list of biosample accessions from NCBI's `prokaryotes.txt` file that it perioidcally updates to run a whole bunch of entrez edirect searches, downloading the xml for hits where a link is found between the biosample and the SRA database.
+
+We chose to include the results in the repo to allow for quicker rerunning. No empty files are commited, so it is important to run the following command prior to committing.  This is to avoid false negatives where a link may exist but was not downloaded due to network connectivity, etc.
+
+```
+find ./output/ncbi_dump/ -empty -type f -delete
+```
+
+# dependencies
+
+- GNU parallel
+- NCBI's entrez direct
+- R
+
+# Executing
+## Fetching the Data
+The main script for fetching data is `./scripts/get_accs.R`, which:
+
+- orchestrates the downloading of a `prokaryotes.txt` file, if one is not found in the current working directory
+- filtering by genome completeness
+- writing out a file of all the commands to run
+- calling those commands in parallel
+- calling the commands to parse the hits
+- merging together runs with multiple datasets
+- saving the output
+
+```
+#$
+#    USAGE: Rscript get_accs.R <"Chromosome|Complete Genome|Contig|Scaffold|All> output/dir/ n_cores_to_use
+Rscript scripts/get_accs.R All ./output/ 16
+
+```
+
+## Plotting the data
+Who are we kidding, you're probably not going to use /my/ plotting scripts are you?
+
+
+
 
 
 
 # Caveats
 
-This ignores the genomes in the list where the chromosome is categorized as a plasmid; there are about 100 of these that list no chromosome, but a megabase-scale plasmid. If someone couldn't get that bit of metadata sorted out, I am hesitent to trust their sequence...
+Don't put too much faith in the nuccore first chromosome columns. This is created with a bunch of regexes of the "replicon" column, which is filled with bad metadata. This also ignores the genomes in the list where the chromosome is categorized as a plasmid; there are about 100 of these that list no chromosome, but a megabase-scale plasmid. If someone couldn't get that bit of metadata sorted out, I am hesitent to trust their sequence...
--- scripts/get_accs.R
+++ scripts/get_accs.R
@@ -2,7 +2,7 @@ args = commandArgs(T)
 
 # test args
 # args=list("Chromosome", "./")
-
+# setwd("~/GitHub/sraFind")
 
 if (!dir.exists(args[2])) dir.create(args[2])
 if(!args[1] %in% c("Chromosome", "Complete Genome", "Contig", "Scaffold", "All")){
@@ -23,16 +23,15 @@ raw <- read.csv(destfile, header=T, sep="\t", stringsAsFactors=FALSE)
 
 db <- raw
 
-if (status %in% c("Complete Genome", "Chromosome")){
-  accs_col = "nuccore_first_chrom"
 # here we select out the first record for each replicon.  We dont' really care if there are multiple chromosome, as they would all come from the same sequencing run/bioproject
 
 # if debugging, add this column for easy viewing of greped vs raw
 #raw$chrom_pre <-raw$Replicons
 #raw$chrom_all <- gsub("chromosome.*?:", "", raw$Replicons)
-  db[, accs_col] <- ifelse(grepl("[cC]hromosome.*?:", db$Replicons),
-                         gsub("(.*?)[/;](.*)", "\\1", gsub("[cC]hromosome.*?:", "", db$Replicons)),
-                         "")
+db[, "nuccore_first_chrom"] <- ifelse(
+  grepl("[cC]hromosome.*?:", db$Replicons),
+  gsub("(.*?)[/;](.*)", "\\1", gsub("[cC]hromosome.*?:", "", db$Replicons)),
+   "")
 # raw$firstchrom <- ifelse(grepl("chromosome:", raw$Replicons),
 #                          gsub("(.*?)[/;](.*)", "\\1", gsub("chromosome:", "", raw$Replicons)),
 #                          "")
@@ -44,7 +43,8 @@ if (status %in% c("Complete Genome", "Chromosome")){
 # # this one could be reopeated with a strip to keep selecting the last of them. You run into issues with CM008567.1-CM008588.1, cause some people just gotta be special
 # raw$addn_last <- gsub(".*chromosome .+?:(.*?)[/.*?;$]", "\\1",  raw$addn_no_plasmids[60])
 
-
+if (status %in% c("Complete Genome", "Chromosome")){
+  accs_col = "nuccore_first_chrom"
   db<- db[db$nuccore_first_chrom != "",]
 } else if (status == "All"){
   accs_col = "Assembly.Accession"
@@ -56,7 +56,7 @@ if (status %in% c("Complete Genome", "Chromosome")){
 print(paste("writing out", nrow(db), accs_col, "of the full", nrow(raw) ))
 write.table(row.names = F, col.names = T, db, sep = "\t",
             file = file.path(args[2], paste0("sraFind-", status, "-prokaryotes.txt")))
-write.table(row.names = F, col.names = F, db[, c(accs_col, "BioSample.Accession")],
+write.table(row.names = F, col.names = F, db[, c("BioSample.Accession", "Assembly.Accession", "nuccore_first_chrom" )],
             file = file.path(args[2], paste0("sraFind-", status, "-biosample-accession.txt")))
 
 ################################################################################
@@ -65,37 +65,44 @@ nice_column_no_run_headers = c("biosample",  "id", "title", "platform",  "instru
 
 ncbi_dir <- file.path(args[2], "ncbi_dump")
 dir.create(ncbi_dir)
-fetch_cmds <- paste0(
+fetch_cmds <- paste0("if [ ! -f ", file.path(ncbi_dir, db$BioSample.Accession), " ] ; then ",
   'esearch -db biosample -query ',  db$BioSample.Accession, ' | ', # get the biosample record
   'elink -target sra | ',  # link it to the SRA database (or try to, it usually fails)
-  'efetch -format docsum > ', file.path(ncbi_dir, db$BioSample.Accession)) # get the summary as XML
+  'efetch -format docsum > ', file.path(ncbi_dir, db$BioSample.Accession), " ; fi") # get the summary as XML
+
+hits_path <- file.path(args[2], "hits.txt")
+
 parse_cmds <- paste0("cat ", file.path(ncbi_dir, db$BioSample.Accession), " | ",
   'xtract ', # use NCBI's tool get tabular data from XML, such as the following colimns
   '-pattern DocumentSummary -element ', paste(ncbi_columns, collapse=" "),
-  ' >> ' , file.path(args[2], "hits.txt"))
-system("cd ~/GitHub/sraFind/test_direct")
+  ' >> ' , hits_path)
+#system("cd ~/GitHub/sraFind/test_direct")
 
 fetch_cmds_path <- file.path(args[2], "sraFind-fetch-cmds.txt")
 print("Writing out fetching commands")
 write.table(row.names = F, col.names = F, fetch_cmds, quote = F,
             file = fetch_cmds_path)
-print(paste0("Running fetch commands using ", cores, "cores"))
-system(paste0("parallel -j ", cores, " :::: ", fetch_cmds_path))
+print(paste0("Running fetch commands using ", cores, " cores"))
+system(paste0("parallel -j ", cores, " --progress :::: ", fetch_cmds_path))
 print("collecting all the hits")
 for (i in head(parse_cmds, n=100)) system(i)
-system("ls")
+#system("ls")
 print("reading hits file")
 
-# we have to allow the last columns to be shaggy to account for biosamples with multiple SRAsand lop them off after the fact.  Most, as we can see heree, have 13 columns.  That is for 1 SRA, size, and availability.  if we have, say, 40, which means we have 9 SRAs, 9 sizes, and 9 availabilities
 
-max_lines = max(count.fields("~/GitHub/sraFind/test_direct/hits.txt", sep="\t", skip = 0,
+
+
+# we have to allow the last columns to be shaggy to account for biosamples with multiple SRAsand lop them off after the fact.  Most, as we can see heree, have 13 columns.  That is for 1 SRA, size, and availability.  if we have, say, 40, which means we have 9 SRAs, 9 sizes, and 9 availabilities
+#hits_path <- "./output/hits.txt"
+max_fields = max(count.fields(hits_path, sep="\t", skip = 0,
+                             blank.lines.skip = TRUE, comment.char = "#"), na.rm = T)
+min_fields = min(count.fields(hits_path, sep="\t", skip = 0,
                              blank.lines.skip = TRUE, comment.char = "#"))
-min_lines = min(count.fields("~/GitHub/sraFind/test_direct/hits.txt", sep="\t", skip = 0,
-                             blank.lines.skip = TRUE, comment.char = "#"))
-table(count.fields("~/GitHub/sraFind/test_direct/hits.txt", sep="\t", skip = 0,
+table(count.fields(hits_path, sep="\t", skip = 0,
                  blank.lines.skip = TRUE, comment.char = "#"))
 
-raw_hits <- read.csv2("~/GitHub/sraFind/test_direct/hits.txt", sep="\t", header=F, fill = T, col.names  = paste(c(1:(max_lines))), stringsAsFactors = F)
+raw_hits <- read.csv2(hits_path, sep="\t", header=F, fill = T, col.names  = paste(c(1:(max_fields))), stringsAsFactors = F)
+## raw_hits <- read.csv2("~/GitHub/sraFind/test_direct/hits.txt", sep="\t", header=F, fill = T, col.names  = paste(c(1:(max_lines))), stringsAsFactors = F)
 
 
 
@@ -109,7 +116,7 @@ hits$nSRAs = as.integer(rowSums("" != table_b & !is.na(table_b)) / 3)
 
 # dont do it!  There must be a better way!
 #                                                     Oh, Im doing it
-# but you know the speed tradeoffs of loops in R
+# but you know the speed tradeoffs of loops in R!
 #                                                   *stubs out cigarette* vectorize /this/
 
 for (i in c(1:nrow(hits))){
@@ -118,13 +125,9 @@ for (i in c(1:nrow(hits))){
   hits[i, "run_SRAs"] <- paste(table_b[i, c(1:nSRAs)], collapse=",")
   hits[i, "run_sizes"] <- paste(table_b[i, c((nSRAs + 1):(2*nSRAs))], collapse=",")
   hits[i, "run_publicities"] <- paste(table_b[i, c((2* nSRAs + 1):(3*nSRAs))], collapse=",")
-  
+
 }
-all_biosamples <- merge(db[, c(accs_col, "BioSample.Accession")], hits, by.x="BioSample.Accession", by.y="biosample")
+all_biosamples <- merge(db[, c("BioSample.Accession", "Assembly.Accession", "Status", "nuccore_first_chrom")], hits, by.x="BioSample.Accession", by.y="biosample", all.x = T)
 
 write.table(row.names = F, col.names = F, all_biosamples,
             file = file.path(args[2], paste0("sraFind-", status, "-biosample-with-SRA.txt")))
-
-
-
-
