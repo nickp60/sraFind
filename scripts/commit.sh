0b6e94953ddb7a26dde57b383fca75cc13a64bd0
nickp60 <nickp60@gmail.com>
2018-09-16 10:48:47 +0100

    tidy up repo, add commit script

---
+++ scripts/commit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "raw hits processed: `ls output/ncbi_dump/ | wc -l`"
+echo "removing empty files"
+find ./output/ncbi_dump/ -empty -type f -delete
+nfile=`ls output/ncbi_dump/ | wc -l`
+echo "non-empty files in output/ncbi_dump: $nfiles"
+
+echo "removing old tar"
+rm ./compressed_ncbi_dump.tar.gz
+echo "creating new tar"
+tar czf ./compressed_ncbi_dump.tar.gz ./output/ncbi_dump/
+git add ./compressed_ncbi_dump.tar.gz
+git commit -m "total of $nfiles files in dump"
+
