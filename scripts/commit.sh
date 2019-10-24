630ae96d6aa4f69256a131ca1aa8bbbb92e2809c
nickp60 <nickp60@gmail.com>
2019-10-24 16:51:44 +0100

    update scripts, remove old results files

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
  echo "counting raw hits"
-nrawfiles=$(find ./output/ncbi_dump/ -type d | wc -l)
-echo "raw hits processed: $nrawfiles"
-echo "copying results for archive"
-rsync --info=progress2 -a --prune-empty-dirs  output/ncbi_dump/ output/ncbi_dump_clean/
-echo "removing empty files and directories"
-find ./output/ncbi_dump_clean/ -empty -type f -delete
-nfiles=$(find ./output/ncbi_dump_clean/ -type f -name "SAM*" | wc -l)
+nrawfiles=$(find ./output/ncbi_dump/ -type f | wc -l)
+echo "raw hits processed: $nrawfiles; deleting empty files"
+find ./output/ncbi_dump/ -empty -delete
+nfiles=$(find ./output/ncbi_dump/  | wc -l)
 echo "non-empty files in output/ncbi_dump: $nfiles"
+echo "copying results for archive"
+rsync --info=progress2 -a  output/ncbi_dump/ output/ncbi_dump_clean/
 
 echo "removing old tar"
-rm ./compressed_ncbi_dump.tar.gz
+rm ./compressed_ncbi_dump.tar.gz 
 echo "creating new tar"
 tar czf ./compressed_ncbi_dump.tar.gz ./output/ncbi_dump_clean/
 git add ./compressed_ncbi_dump.tar.gz
